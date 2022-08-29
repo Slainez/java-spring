5e3d3babdfdb303bc7c2c5be361ec18f57100c43
Slainez <sullyvan360@gmail.com>
2022-08-29 14:27:45 +0200

    application properties

--- src/main/java/fr/m2i/javaspring/JavaspringApplication.java
+++ src/main/java/fr/m2i/javaspring/JavaspringApplication.java
@@ -4,15 +4,25 @@ import fr.m2i.javaspring.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class JavaspringApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(JavaspringApplication.class, args);
+            
+            
+                      ConfigurableEnvironment env = new StandardEnvironment();
+                      env.setActiveProfiles("dev");
+                      
+                      SpringApplication sa = new SpringApplication(JavaspringApplication.class);
+                      sa.setEnvironment(env);
+        
+	       ApplicationContext ctx = sa.run(args);
                 
-                            DataSourceConfig dataConfig = ctx.getBean(DataSourceConfig.class);
-                            dataConfig.setup();
+                     DataSourceConfig dataConfig = ctx.getBean(DataSourceConfig.class);
+                     dataConfig.setup();
 	}
 
 }
